8401e21d767003bbf27a6538c897beba1de32e16
Ogabek Yusupov <ogabekyusupov@gmail.com>
2020-10-16 23:35:47 +0100

    1502 solution

---
+++ Easy/1502. Can Make Arithmetic Progression From Sequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr = sorted(arr)
+        diff = arr[1] - arr[0]
+        for i in range(len(arr) - 1):
+            if arr[i + 1] - arr[i] != diff:
+                return False
+        return True
