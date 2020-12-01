78eb9f782f3dfa5cf33743079173cc48cd45bb6b
Ogabek Yusupov <ogabekyusupov@gmail.com>
2020-12-01 21:41:33 +0000

    1550 solution

---
+++ Easy/1550. Three Consecutive Odds.py
@@ -0,0 +1,10 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odds = 0
+        for n in arr:
+            if n & 1 != 0:
+                odds+=1
+                if odds == 3:
+                    return True
+            else:
+                odds = 0
