27d73f4a528dca72baff9668730f5219610845fb
Ogabek Yusupov <ogabekyusupov@gmail.com>
2020-12-16 21:00:44 +0000

    solution 112

---
+++ Easy/112. Path Sum.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if root == None:
+            return False
+        if root.left == None and root.right == None:
+            return root.val == sum
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
