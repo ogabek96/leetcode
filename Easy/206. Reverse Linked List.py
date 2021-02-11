9767659b66b99e3b53cb6cb620fab3670c0835c3
Ogabek Yusupov <ogabekyusupov@gmail.com>
2021-02-11 13:46:38 +0000

    update 206 solution

--- Easy/206. Reverse Linked List.py
+++ Easy/206. Reverse Linked List.py
@@ -5,16 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        numbers = []
-        if(head == None):
-            return None
-        while head != None:
-            numbers.append(head.val)
-            head = head.next
-        first = ListNode(numbers[len(numbers) - 1])
-        current = first
-        for i in reversed(numbers[:-1]):
-            newNode =  ListNode(i)
-            current.next = newNode
-            current = newNode
-        return first
+        curr = head
+        prev = None
+        next = None
+        while curr != None:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
